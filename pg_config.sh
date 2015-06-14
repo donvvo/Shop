c2ad7966f5b45e382ff7b102f17d70d62accfcf5
dxlin <danielx.lin@gmail.com>
2015-06-13 22:48:37 -0400

    added extra development tools needed for amazon api

--- pg_config.sh
+++ pg_config.sh
@@ -4,6 +4,8 @@ apt-get -qqy install libpq-dev
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
+apt-get -qqy install libxml2-dev
+apt-get -qqy install libxslt1-dev
 sudo pip install -r /vagrant/requirements/local.txt
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
